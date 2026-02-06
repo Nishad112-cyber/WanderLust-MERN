4598f066faeac9df0559a85374dac889eea83ac8
Nishad112-cyber <nishadguptad@gmail.com>
2026-02-06 20:04:14 +0530

    Added backend app.js and listing views templates

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express= require("express");
 const app= express();
 const Listing = require("./models/listing.js");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const mongoose = require("mongoose");
+const path = require("path");
 
 const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -14,28 +19,51 @@ main()
 async function main(){
    await mongoose.connect(MONGO_URL);
 }
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req,res)=>{
     res.send("i am ready to get request ")
 
 });
-app.get("/testlisting",async (req,res)=>{
-    let samplelisting= new Listing({
-        title:"my new villa",
-        description:"by the beach",
-        image:"nothing",
-        price:6000,
-        location: "goa",
-        country:"india",
 
-    });
-
-    await samplelisting.save();
-    console.log("sample test");
-    res.send("success ");
-
+ // new rout 
+ app.get("/listing/new", (req,res)=>{
+ res.render("listings/new.ejs")   
 });
+//index rout 
+app.get("/listings",async (req,res)=>{
+ const allListings=  await  Listing.find({})
+ res.render("listings/index", {allListings});
+});
+
+//show rout 
+app.get("/listings/:id", async(req,res)=>{
+    let {id}= req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show", {listing});
+})
+
+//create rout
+app.post("/listings",async (req, res)=>{
+const newlisting= new Listing(req.body.listings)
+  await newlisting.save();
+res.redirect("/listings");
+});
+
+// app.get("/testlisting",async (req,res)=>{
+//     let samplelisting= new Listing({
+//         title:"my new villa",
+//         description:"by the beach",
+//         image:"nothing",
+//         price:6000,
+//         location: "goa",
+//         country:"india",
+//     });
+//     await samplelisting.save();
+//     console.log("sample test");
+//     res.send("success ");
+// });
 
 
 app.listen(9000, ()=>{
