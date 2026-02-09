f4fa053d6bf0a9b17abfd568de1963f6760b16a2
Nishad112-cyber <nishadguptad@gmail.com>
2026-02-09 08:29:17 +0530

    added new feature

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express= require("express");
+const path = require("path");
 const app= express();
 const Listing = require("./models/listing.js");
 const methodOverride= require("method-override");
@@ -8,9 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname ,"/public")))
 
 const mongoose = require("mongoose");
-const path = require("path");
+
 
 const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust";
 
