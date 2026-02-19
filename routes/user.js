0dc4039582b0dc3457ff51319b89ed8659c7b0fd
Nishad112-cyber <nishadguptad@gmail.com>
2026-02-19 16:16:31 +0530

    to add signup

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,26 @@
 const express= require("express");
 const router= express.Router();
-const User= require("../models/user.js");
+const user= require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 
 router.get("/signup" ,(req, res) =>{
     res.render("users/signup.ejs")
 })
 
-router.post("/signup" , async (req,res,next) =>{
-    try{
-    let{username, email, password}= req.body;
-   const newUser=  new User({email, username});
- const  registerUser=  await User.register(newUser, password);
-//    console.log(registerUser);
-   req.flash("success", "welocme to wanderlust ");
-   res.redirect("/listings")
+// router.post("/signup" , async (req,res,next) =>{
+//     try{
+//     let{username, email, password}= req.body;
+//    const newUser=  new User({email, username});
+//  const  registerUser=  await User.register(newUser, password);
+// //    console.log(registerUser);
+//    req.flash("success", "welocme to wanderlust ");
+//    res.redirect("/listings")
 
-   } catch(err){
-        console.log("🔥 SIGNUP ERROR 🔥");
-        console.log(err);          // full error object
-        console.log(err.stack);    // full stack trace
-        next(err);
-    }
-});
+//    } catch(err){
+//         console.log("🔥 SIGNUP ERROR 🔥");
+//         console.log(err);          // full error object
+//         console.log(err.stack);    // full stack trace
+//         next(err);
+//     }
+// });
 module.exports= router;
