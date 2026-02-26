7878a26d958d3abd64fdae61580d98a0d5fbc68f
Nishad112-cyber <nishadguptad@gmail.com>
2026-02-26 17:38:59 +0530

    added login feature

--- routes/user.js
+++ routes/user.js
@@ -9,39 +9,65 @@ const userController = require("../controllers/users.js")
 
 
 router.route("/signup")
-.get((userController.renderSignupform))
+.get(userController.renderSignupform)
 .post(
-     wrapAsync(async (req,res,next) =>{
-        try{
+  wrapAsync(async (req, res, next) => {
+    try {
+      let { username, email, password } = req.body;
 
-            let{username, email, password}= req.body;
-           const newUser=  new User({email, username});
-           const  registeredUser=  await User.register(newUser, password);
-           req.login(registeredUser, (err) =>{
-            if(err){
-              return next(err);
-            }
-            req.flash("success", "you are login now");
-            res.redirect("/listings");
-           });
-          
-        } catch(e){
-            req.flash("error", e.message);
-            res.redirect("/signup");
-        }
-}));
+      // ✅ only allowed admin credentials
+      const allowedUsername = "DileepNishadg";
+      const allowedEmail = "nishadguptad@gmail.com";
+        const allowedPassword = "Nishad!@56d";  
+
+
+      if (username !== allowedUsername || email !== allowedEmail) {
+        req.flash("error", "Signup not allowed");
+        return res.redirect("/signup");
+      }
+       if (password !== allowedPassword) {   // 👈 yaha add karo
+        req.flash("error", "Signup not allowed");
+        return res.redirect("/signup");
+      }
+
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+
+      req.login(registeredUser, (err) => {
+        if (err) return next(err);
+        req.flash("success", "You are logged in now");
+        res.redirect("/listings");
+      });
+
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
+);
 
 router.route("/login")
-.get((req,res) =>{
-    res.render("users/login.ejs")
+.get((req, res) => {
+  res.render("users/login.ejs");
 })
 .post(
   saveRedirectUrl,
+
+  // ✅ allow only admin username
+  (req, res, next) => {
+    if (req.body.username !== "admin") {
+      req.flash("error", "Invalid credentials");
+      return res.redirect("/login");
+    }
+    next();
+  },
+
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
-   userController.afterlogin
+
+  userController.afterlogin
 );
 
 router.get("/logout",(userController.logout) );
